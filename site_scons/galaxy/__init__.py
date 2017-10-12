c34f0184895efaa0ff4d2be79b549f242470a46c
wuxiangwei <shanno@yeah.net>
2017-10-12 11:24:58 +0800

    删除galaxy目录

--- site_scons/galaxy/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- mode: python; -*-
-# coding:utf-8
-
-def print_build_failures():
-    from SCons.Script import GetBuildFailures
-    for bf in GetBuildFailures():
-        print "[GALAXY] %s failed: %s" % (bf.node, bf.errstr)
